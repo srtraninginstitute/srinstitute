add49e40f8797a623389cf26bd6ae03987b34f5f
SHARUKH MAHAMMAD <sharukhmahammad@SHARUKHs-MacBook-Air.local>
2025-04-05 18:46:44 +0530

    build changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    minify: 'terser',
+    cssMinify: true,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
 })
